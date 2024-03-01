42fd80720d1594c9c402d6f18fdc9f539baacfcf
Jaime Vargas <83519750+jaimevargas315@users.noreply.github.com>
2024-03-01 21:39:04 +0000

    finished

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
+require "asciiart"
+require 'asciiart'
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+
+  cat = AsciiArt.new("https://cdn.shopify.com/s/files/1/0613/5054/3550/files/how-to-draw-a-cat_480x480.webp?v=1690335501")
+  @example = cat.to_ascii_art(format:"html", color: true, width:100)
+
+  erb(:main)
+end
+
+get("/results") do
+
+  link = params.fetch('image')
+
+  a = AsciiArt.new(link)
+  @art = a.to_ascii_art(format:"html", color: true, width:100)
+
+  erb(:results)
 end
